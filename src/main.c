3e26480c45c12e8b44eeb01a4f3404adb3b27a46
gustavopcr <gustavopc2501@gmail.com>
2024-09-20 17:11:33 -0300

    experimentando funcao de click em botao

--- src/main.c
+++ src/main.c
@@ -28,23 +28,66 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+#define NUM_FRAMES 3
 int main ()
 {
 	// Tell the window to use vysnc and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	const int screenWidth = 1200;
+    const int screenHeight = 800;
+	InitWindow(screenWidth, screenHeight, "Hello Raylib");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
+	Texture2D button = LoadTexture("line.png"); // Load button texture
+
+	// Define frame rectangle for drawing
+    float frameHeight = (float)button.height/NUM_FRAMES;
+    Rectangle sourceRec = { 0, 0, (float)button.width, frameHeight };
+	// Define button bounds on screen
+    Rectangle btnBounds = { screenWidth/2.0f - button.width/2.0f, screenHeight/2.0f - button.height/NUM_FRAMES/2.0f, (float)button.width, frameHeight };
+
+    int btnState = 0;               // Button state: 0-NORMAL, 1-MOUSE_HOVER, 2-PRESSED
+    bool btnAction = false;         // Button action should be activated
+
+    Vector2 mousePoint = { 0.0f, 0.0f };
+
+    SetTargetFPS(60);
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
+		mousePoint = GetMousePosition();
+        btnAction = false;
+
+		// Check button state
+        if (CheckCollisionPointRec(mousePoint, btnBounds))
+        {
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) btnState = 2;
+            else btnState = 1;
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnAction = true;
+        }
+        else btnState = 0;
+
+        if (btnAction)
+        {
+            for(int i=0; i<100; i++){
+				DrawPixel(100+i, 100+i, PURPLE);
+			}
+
+            // TODO: Any desired action
+        }
+
+        // Calculate button frame rectangle to draw depending on button state
+        sourceRec.y = btnState*frameHeight;
+        //----------------------------------------------------------------------------------
+
+
 		// drawing
 		BeginDrawing();
 
@@ -52,10 +95,10 @@ int main ()
 		ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylibaaaaaaaa", 200,200,20,WHITE);
-
+		DrawText("Hello Raylib", 200,200,20,BLACK);
+		DrawTextureRec(button, sourceRec, (Vector2){ btnBounds.x, btnBounds.y }, WHITE); // Draw button frame
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		//DrawTexture(wabbit, 400, 200, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -64,6 +107,7 @@ int main ()
 	// cleanup
 	// unload our texture so it can be cleaned up
 	UnloadTexture(wabbit);
+	UnloadTexture(button);
 
 	// destory the window and cleanup the OpenGL context
 	CloseWindow();
